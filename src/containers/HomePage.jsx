4bf844d8caa50364f072be3194ae4a49248bd988
Jordan Brown <jordan@Jordans-Air.attlocal.net>
2017-08-04 14:04:40 -0700

    phase 1

---
+++ src/containers/HomePage.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+
+    render() {
+
+        return(
+            
+            <header className='header'>
+                <h1>The Jordan Chronicles</h1>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Family</li>
+                        <li>Blog</li>
+                    </ul>
+                </nav>
+            </header>
+        )
+
+    }
+}
+
+export default HomePage;
+
