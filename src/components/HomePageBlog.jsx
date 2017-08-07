2a6f441a12179bb186c2194d93e4c677cff94452
Jordan Brown <jordan@Jordans-Air.attlocal.net>
2017-08-06 17:40:36 -0700

    HomePage

--- src/components/HomePageBlog.jsx
+++ src/components/HomePageBlog.jsx
@@ -1,4 +1,26 @@
 import React from 'react';
 
 
-const HomePageBlog = props =>
+const HomePageBlog = props => {
+//    creating a variable that receives BlogPost object from props
+    const blogPost = (props) => {
+        
+    };
+
+    return (
+        
+        <div>
+
+            {/* Image and Title of HomePage BlogPost  */}
+            <div>  
+                <img src={}/>
+                <h2></h2>
+            </div>
+           
+           {/* Content of BlogPost  */}
+            <div>
+                <h3></h3>
+            </div>
+        </div>
+    )
+}
