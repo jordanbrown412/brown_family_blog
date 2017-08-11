176ac8670cc3273fb19cc294821ddd2ee018ae61
Jordan Brown <jordan@Jordans-Air.attlocal.net>
2017-08-11 16:33:50 -0700

    CmS Form skeleton with Sidebar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import HomePage from './containers/HomePage';
-import './App.css';
+import Sidebar from './containers/Sidebar';
+// import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <HomePage />
+        {/* <Header />
+        <HomePage /> */}
+        <Sidebar />
       </div>
     );
   }
