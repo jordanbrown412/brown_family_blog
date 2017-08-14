b43d15806325f553ac75a8e1c963d941c3240348
Jordan Brown <jordan@Jordans-Air.attlocal.net>
2017-08-14 00:05:17 -0700

    Login

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import HomePage from './containers/HomePage';
 import Sidebar from './containers/Sidebar';
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 
-        <Header />
-        {/* <Header />
-        <HomePage /> */}
-        <Sidebar />
+      <LoginForm/>
 
       </div>
     );
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
-
-import { Card } from 'semantic-ui-react';
+import { Card, Form, Input, Segment, Container, Grid, Header } from 'semantic-ui-react';
+import '../App.css';
 
 const LoginForm = () => (
-    <Card>
-        <Card.Content>
-            <Card.Header>
-                Login
-            </Card.Header>
+    
+    <div>
+    <Grid verticalAlign='middle' centered columns={1} className='login'>
+    <Grid.Column width={7}>
+    {/* <Segment basic> */}
+    <Card fluid className='login'>
+        <Card.Content className='login-header'>
+            
+            <Header as='h2'textAlign='center'>
+                Login Page
+            </Header>
+            
         </Card.Content>
-        <Card.Content></Card.Content>
+
+        <Card.Content>
+            <Form>
+                <Form.Field control={Input} label='Username' placeholder='Username' />
+                <Form.Field control={Input} label='Password' placeholder='password' />
+            </Form>
+        </Card.Content>
     </Card>
+    {/* </Segment> */}
+    </Grid.Column>
+    </Grid>
+    </div>
 )
+
+export default LoginForm;
