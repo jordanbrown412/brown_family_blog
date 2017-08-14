9f6091b45a4b5777adcb5e1f8fa9818a37afbf03
Jordan Brown <jordanbrown412@gmail.com>
2017-08-13 21:59:02 -0700

    initialize Login

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Card } from 'semantic-ui-react';
+
+const LoginForm = () => (
+    <Card>
+        <Card.Content>
+            <Card.Header>
+                Login
+            </Card.Header>
+        </Card.Content>
+        <Card.Content></Card.Content>
+    </Card>
+)
--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
-            
-        )
+            <Router>
+                <Switch>
+                    <Route exact={true} path='/' component={FamilyCard} />
+                    <Route exact={true} path='/cms' component={CmsRouter} />
+                    <Route exact={true} path='/blog' component={BlogRouter} />
+                    <Route exact={true} path='/news' component={News} />
+                </Switch> 
+            </Router>
+        );
     }
 }
